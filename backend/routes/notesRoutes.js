e60e518fc73eee25e93aadfe1a0313cb1cc32c64
Fatih Yazicioglu <fatihyaziciweb@gmail.com>
2026-06-11 18:16:52 +0200

    Controllers optimized

---
+++ backend/routes/notesRoutes.js
@@ -0,0 +1,13 @@
+import express from "express";
+import {createNote, getAllNotes, updateNote,deleteNote } from "../controllers/notesControllers.js";
+const router = express.Router();
+
+router.get("/", getAllNotes);
+
+router.post("/", createNote);
+    
+router.delete("/:id", deleteNote);
+router.put("/:id", updateNote           );
+
+
+export default router;  
