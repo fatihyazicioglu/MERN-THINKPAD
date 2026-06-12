ae53132584a3be7d131ee6be8afa8f113159305d
Fatih Yazicioglu <fatihyaziciweb@gmail.com>
2026-06-12 03:08:15 +0200

    Database Connected

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,19 @@
 import express from "express";
-import notesRoutes from "./routes/notesRoutes.js";
+import notesRoutes from "./src/routes/notesRoutes.js";
+import { connectDB } from "./src/config/db.js";
+import dotenv from "dotenv";
 
-
+dotenv.config({ path: '.env.local', quiet: true });
+console.log("MONGO_URI:", process.env.MONGO_URI);
 const app = express();
+const PORT = process.env.PORT || 5001;
+
+connectDB();
 
 app.use("/api/notes", notesRoutes);
 
 
-app.listen(5001, () => {
-    console.log("Server is running on port 5001");
+app.listen(PORT, () => {
+    console.log("Server is running on port:", PORT);
 });
 
