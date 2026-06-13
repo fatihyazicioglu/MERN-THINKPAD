00c1745dc0c2cc3aae7113b78003e008958cd1ea
Fatih Yazicioglu <fatihyaziciweb@gmail.com>
2026-06-13 14:21:30 +0200

    Middleware,Redis,Upstash,RateLimit Added

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import notesRoutes from "./src/routes/notesRoutes.js";
 import { connectDB } from "./src/config/db.js";
+import rateLimiter from "./src/middleware/rateLimiter.js";
 import dotenv from "dotenv";
 
 dotenv.config({ path: '.env.local', quiet: true });
@@ -8,12 +9,18 @@ console.log("MONGO_URI:", process.env.MONGO_URI);
 const app = express();
 const PORT = process.env.PORT || 5001;
 
-connectDB();
+/* connectDB(); */
 
 app.use(express.json());
+app.use(rateLimiter); // Apply rate limiter middleware to all routes    
 app.use("/api/notes", notesRoutes);
 
-app.listen(PORT, () => {
+/* app.listen(PORT, () => {
     console.log("Server is running on port:", PORT);
-});
+}); */
 
+connectDB().then(() => {
+  app.listen(PORT, () => {
+    console.log("Server started on PORT:", PORT);
+  });
+});
