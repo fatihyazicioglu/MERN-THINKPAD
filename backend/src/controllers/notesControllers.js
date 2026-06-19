f7f50de12b8f098b041f20da532d57992a30dd7a
Fatih Yazicioglu <fatihyaziciweb@gmail.com>
2026-06-19 23:49:08 +0200

    NoteDetailPage added

--- backend/src/controllers/notesControllers.js
+++ backend/src/controllers/notesControllers.js
@@ -22,6 +22,19 @@ export async function createNote(req, res) {
   }
 }
 
+export async function getNoteById(req, res) {
+  try {
+    const note = await Note.findById(req.params.id);
+
+    if (!note) return res.status(404).json({ message: "Note not found" });
+
+    res.status(200).json(note);
+  } catch (error) {
+    console.error("Error in getNoteById controller", error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+}
+
 export async function updateNote(req, res) {
   try {
     const { title, content } = req.body;
--- backend/src/routes/notesRoutes.js
+++ backend/src/routes/notesRoutes.js
@@ -3,12 +3,14 @@ import rateLimiter from "../middleware/rateLimiter.js";
 import {
   createNote,
   getAllNotes,
+  getNoteById,
   updateNote,
   deleteNote,
 } from "../controllers/notesControllers.js";
 const router = express.Router();
 
 router.get("/", getAllNotes);
+router.get("/:id", getNoteById);
 
 router.post("/", rateLimiter, createNote);
 
